0462dbeb6802375e654f6ef41240dde50c79ca5e
Renier Meintjies <50550150+RenierMeintjies@users.noreply.github.com>
2023-05-25 13:55:30 +0200

    WIP

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 export default function Landing() {
   return (
-    <h1>
-      Hello <span class="text-gradient">World!</span>
-    </h1>
+    <div className="shadow bg-white">
+      <div className="h-16 mx-auto px-5 flex items-center justify-between">
+        <img src="/logo-no-background.svg" className="w-24" />
+
+        <ul className="flex items-center gap-5">
+          <li>
+            <a className="hover:text-cyan-500 transition-colors" href="">
+              Work
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-cyan-500 transition-colors" href="">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-cyan-500 transition-colors" href="">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
