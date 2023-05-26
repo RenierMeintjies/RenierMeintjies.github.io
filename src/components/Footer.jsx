34b3c87c3a64980c1ce7bd686fb72e9d03b2762a
Renier Meintjies <50550150+RenierMeintjies@users.noreply.github.com>
2023-05-26 10:57:32 +0200

    delete files

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  const year = new Date().getFullYear();
-
-  return (
-    <footer className="text-center border-t bg-white p-1">{`Copyright © Renier Meintjies ${year}`}</footer>
-  );
-}
